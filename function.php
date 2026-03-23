469bc794da482e1eb837f54a41c027a09dd69a11
Some25Coding <soressaaddisu2@gmail.com>
2026-03-23 14:58:14 +0300

    Add files via upload

---
+++ function.php
@@ -0,0 +1,38 @@
+<?php
+function sum() {
+    $x=8;
+    $y=12;
+    echo"the sum of $x and $y is" . $x+$y;
+}
+sum();
+function dif() {
+    $x=8;
+    $y=12;
+    echo"the difference of $x and $y is" . $x-$y;
+}
+dif();
+function mul() {
+    $x=8;
+    $y=12;
+    echo"the multiple of $x and $y is" . $x+$y;
+}
+mul();
+function div() {
+    $x=8;
+    $y=12;
+    echo"the quotient of $x and $y is" . $x/$y;
+}
+div();
+function rem() {
+    $x=8;
+    $y=12;
+    echo"the remainder of $x and $y is" . $x%$y;
+}
+rem();
+function pow() {
+    $x=8;
+    $y=12;
+    echo"the power of $x and $y is" . $x*$y;
+}
+pow();
+?>
