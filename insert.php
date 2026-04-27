9c52257c8b7939e042e6ee07f5b711a76715da33
Some25Coding <soressaaddisu2@gmail.com>
2026-04-27 23:45:34 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,20 +1,22 @@
 <?php
 require('connection.php');
-//echo "<br>";
-if(isset($_POST['submit']))
-{
-$id=$_POST['id'];
-$name=$_POST['name'];
-$age=$_POST['age'];
-if(empty($id)||empty($name)||empty($age)){
-echo "<font color='red'>id,name or age is empty";
-include("add.html");
-die();}
-$insert="insert into stud (id,name,age) values('$id','$name','$age')";
-$result=mysqli_query($conn,$insert);
-echo "<font color='green'>data added successfully";
 echo "<br>";
-include('read.php');
+
+if(isset($_POST['submit'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    
+    if(empty($id) || empty($name) || empty($age)){
+        echo "<font color='red'>id, name or age is empty</font><br>";
+        include("add.html");
+        die();
+    }
+    
+    $insert = "insert into Student (id,name,age) values('$id','$name','$age')";
+    $result = mysqli_query($conn, $insert);
+    
+    echo "<font color='green'>data added successfully</font><br>";
+    include('read.php');
 }
 ?>
-
