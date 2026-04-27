995d42614ba3d72618879c92f2dfa8f2e8cee392
Some25Coding <soressaaddisu2@gmail.com>
2026-04-27 23:45:13 +0300

    Update read.php

--- read.php
+++ read.php
@@ -1,25 +1,27 @@
 <?php
 require('connection.php');
-$select="select * from stud";
-$result=mysqli_query($conn,$select);
+$select = "select * from Student";
+$result = mysqli_query($conn, $select);
 ?>
 <html>
 <head>
 <title>read data</title>
 </head>
 <body>
-<a href="add.html">add new form1</a><br>
+<br>
+<a href="add.html">add new form1</a><br><br>
 <table border="2">
 <tr bgcolor="grey"><td> id : </td><td> name : </td><td> age : </td><td>update</td></tr>
 <?php
-while($r=mysqli_fetch_array($result)){
-echo "<tr>";
-echo "<td>".$r['id']."</td>";
-echo "<td>".$r['name']."</td>";
-echo "<td>".$r['age']."</td>";
-echo "<td><a href=\"update.php?id=$r[id]\">edit</a>|
-<a href=\"delete.php?id=$r[id]\" onclick=\"return confirm(' sure to delete?')\">delete</a></td>";
-echo "</tr>";}
+while($r = mysqli_fetch_array($result)){
+    echo "<tr>";
+    echo "<td>".$r['id']."</td>";
+    echo "<td>".$r['name']."</td>";
+    echo "<td>".$r['age']."</td>";
+    echo "<td><a href=\"update.php?id=".$r['id']."\">edit</a> | 
+    <a href=\"delete.php?id=".$r['id']."\" onclick=\"return confirm(' sure to delete?')\">delete</a></td>";
+    echo "</tr>";
+}
 ?>
 </table>
 </body>
