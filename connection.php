00b0634d86266f6d6bf6b38d10362e8fc1a8108d
Some25Coding <soressaaddisu2@gmail.com>
2026-04-27 23:42:28 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,14 @@
 <?php
-$dbhost="localhost";
-$dbusername="root";
-$dbpassword='';
-$dbname="practice";
+$dbhost = "localhost";
+$dbusername = "root";
+$dbpassword = '';
+$dbname = "Practice";
 
-$conn=mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname);
+$conn = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
 
 if(!$conn){
-die("connection error: ".mysqli_connect_error());
+    die("connection error: " . mysqli_connect_error());
 }
 
 echo "connection successful";
-
 ?>
