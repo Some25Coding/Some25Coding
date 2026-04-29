64433ce153dedfb75e16fad36f1f228766107f3a
Some25Coding <soressaaddisu2@gmail.com>
2026-04-29 22:12:01 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
 require('connection.php');
 $id = $_GET['id'];
-$r = mysqli_query($conn, "delete from Student where id='$id'");
+$r = mysqli_query($conn, "delete from employee where E_ID='$id'");
 include('read.php');
 ?>
