9d86cdd02e55033813f3f455e648a01902aae09c
Some25Coding <soressaaddisu2@gmail.com>
2026-04-29 22:10:35 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,17 +3,18 @@ require('connection.php');
 echo "<br>";
 
 if(isset($_POST['submit'])) {
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $age = $_POST['age'];
+    $e_id = $_POST['E_ID'];
+    $e_name = $_POST['E_Name'];
+    $e_position = $_POST['E_Position'];
+    $e_salary = $_POST['E_Salary'];
     
-    if(empty($id) || empty($name) || empty($age)){
-        echo "<font color='red'>id, name or age is empty</font><br>";
+    if(empty($e_id) || empty($e_name) || empty($e_position) || empty($e_salary)){
+        echo "<font color='red'>ID, Name, Position, or Salary is empty</font><br>";
         include("add.html");
         die();
     }
     
-    $insert = "insert into Student (id,name,age) values('$id','$name','$age')";
+    $insert = "insert into employee (E_ID, E_Name, E_Position, E_Salary) values('$e_id','$e_name','$e_position','$e_salary')";
     $result = mysqli_query($conn, $insert);
     
     echo "<font color='green'>data added successfully</font><br>";
